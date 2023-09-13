92009d2bc6239e1f514405f79774dbc68d6dc558
gobisri51569023 <144772519+gobisri51569023@users.noreply.github.com>
2023-09-13 02:46:49 -0700

    player.py

---
+++ player.py
@@ -0,0 +1,12 @@
+def play(self):
+    print("the player is playing cricket.")
+class batsman(player):
+    def play(self):
+    print("the batsman is batting.")
+class bowler(player):
+      def play(seif):
+          printf("the bowler is bowling.")
+#creating objects of the batsman and bolwer classes batsman = batsman() bowler = bowler()
+# calling the play() method for each object 
+batsman.play()
+bowler.play()
